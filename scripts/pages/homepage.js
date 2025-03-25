738ad4f26b9666b8837feb8342a93e0b4ad20925
Macbook pro 2021 <macbookpro2021@MacBook-Pro-de-Macbook.local>
2025-03-25 02:15:01 +0100

    Updated files and fixed issues

---
+++ scripts/pages/homepage.js
@@ -0,0 +1,48 @@
+import initAboutTeaser, {
+  killAboutTeaser,
+} from "https://zakibouchebtoul.github.io/ZakiBouchebtoul/scripts/features/general/aboutTeaser.js";
+import initAwardListHover, {
+  killAwardListHover,
+} from "https://zakibouchebtoul.github.io/ZakiBouchebtoul/scripts/features/general/awardListHover.js";
+import initAwardListScroll, {
+  killAwardListScroll,
+} from "https://zakibouchebtoul.github.io/ZakiBouchebtoul/scripts/features/general/awardListScroll.js";
+import initCardList, { killCardList } from "https://zakibouchebtoul.github.io/ZakiBouchebtoul/scripts/features/general/cardList.js";
+import initConnectScroll, {
+  killConnectScroll,
+} from "https://zakibouchebtoul.github.io/ZakiBouchebtoul/scripts/features/general/connectScroll.js";
+import initFooter, { killFooter } from "https://zakibouchebtoul.github.io/ZakiBouchebtoul/scripts/features/general/footer.js";
+import initHeadlineScroll, {
+  killHeadlineScroll,
+} from "https://zakibouchebtoul.github.io/ZakiBouchebtoul/scripts/features/general/headlineScroll.js";
+import initProjectTeaser, {
+  killProjectTeaser,
+} from "https://zakibouchebtoul.github.io/ZakiBouchebtoul/scripts/features/general/projectTeaser.js";
+import initTextOnScroll, {
+  killTextOnScroll,
+} from "https://zakibouchebtoul.github.io/ZakiBouchebtoul/scripts/features/general/textOnScroll.js";
+import initAchievement, {
+  killAchievement,
+} from "https://zakibouchebtoul.github.io/ZakiBouchebtoul/scripts/features/homepage/achievement.js";
+import initPattern, { killPattern } from "https://zakibouchebtoul.github.io/ZakiBouchebtoul/scripts/features/homepage/pattern.js";
+import initReducedTeaser, {
+  killReducedTeaser,
+} from "https://zakibouchebtoul.github.io/ZakiBouchebtoul/scripts/features/listPage/reducedTeaser.js";
+import shared from "./shared.js";
+function init() {
+  initAwardListHover(),
+
+    initCardList()
+
+}
+function mobileInit() {
+    initCardList()
+  
+}
+function cleanup() {
+ 
+}
+function mobileCleanup() {
+
+}
+export default { init, cleanup, mobileInit, mobileCleanup };
