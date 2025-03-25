5908f161c75976e3d2e6d492285dec9e45315f87
zakibouchebtoul <119544461+zakibouchebtoul@users.noreply.github.com>
2025-03-25 02:32:37 +0100

    Update secondsection.js

--- scripts/secondsection.js
+++ scripts/secondsection.js
@@ -55,7 +55,7 @@ function getBaseUrl() {
 function loadPageModule(e) {
   const t = getBaseUrl();
   const baseUrl = window.location.origin;
-import(`${baseUrl}/scripts/pages/home-page.js`)
+import(`${baseUrl}/scripts/pages/homepage.js`)
 
     .then((t) => {
       (currentAnimationModule = t.default || {}),
